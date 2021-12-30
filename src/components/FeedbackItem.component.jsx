a33b1f68a8d06c3c1192615528cc950627f17ecf
Roberts Peirāgs <rpeirags@gmail.com>
2021-12-30 13:32:02 +0100

    Delete feedback

--- src/components/FeedbackItem.component.jsx
+++ src/components/FeedbackItem.component.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types'
 
+import * as Icon from 'react-icons/fa'
+
 import Card from './shared/Card.component'
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <Icon.FaTimes color='purple' />
+      </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   )
