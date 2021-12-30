64a4c93ea958b83630bd921b5f994534cb7307fa
Roberts Peirāgs <rpeirags@gmail.com>
2021-12-30 06:08:51 +0100

    Card + proptypes

--- src/components/FeedbackList.component.jsx
+++ src/components/FeedbackList.component.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 import FeedbackItem from './FeedbackItem.component'
 
 const FeedbackList = ({ feedback }) => {
@@ -12,6 +14,16 @@ const FeedbackList = ({ feedback }) => {
       ))}
     </div>
   )
+}
+
+FeedbackList.prototype = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
 }
 
 export default FeedbackList
